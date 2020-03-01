5dd473ea2740394cb13bf0aa8df8c76fb2ba45e2
Vorbert Kruk <pryrokopter@gmail.com>
2020-03-01 21:23:41 +0100

    Added functionality related to the observing DOM for changes, and using the rendered button to accept the modal.

--- hidingFunctionality.js
+++ hidingFunctionality.js
@@ -1,3 +1,43 @@
+Array.prototype.isNotEmpty = function() {
+  return this.length > 0;
+};
+
 (() => {
-  console.log('hey');
+  const popupContainerName = 'ytd-popup-container';
+  const modalContainerName = 'paper-dialog';
+  const confirmButtonId = 'confirm-button';
+
+  const clickAcceptButton = () => {
+    const confirmButton = document.querySelector(
+      `${modalContainerName} #${confirmButtonId}`,
+    );
+    confirmButton && confirmButton.click();
+  };
+
+  !window.modalObserver &&
+    (() => {
+      const popupContainer = document.querySelector(popupContainerName);
+
+      window.modalObserver = new MutationObserver(mutations => {
+        const modalExists = [...mutations]
+          .filter(mutationList =>
+            [...mutationList.addedNodes]
+              .filter(
+                mutation =>
+                  mutation.nodeName.toLowerCase() === modalContainerName,
+              )
+              .isNotEmpty(),
+          )
+          .isNotEmpty();
+
+        modalExists && clickAcceptButton();
+      });
+
+      window.modalObserver.observe(popupContainer, {
+        childList: true,
+        subtree: true,
+        attributes: false,
+        characterData: false,
+      });
+    })();
 })();

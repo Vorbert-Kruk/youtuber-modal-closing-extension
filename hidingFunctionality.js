42dcf70e89349b199c1e9c93a87be79ecb6af7d8
Vorbert Kruk <pryrokopter@gmail.com>
2020-06-24 23:19:24 +0200

    Dodano funkcjonalność wyłącznia reklam

--- hidingFunctionality.js
+++ hidingFunctionality.js
@@ -1,43 +1,42 @@
-Array.prototype.isNotEmpty = function() {
-  return this.length > 0;
-};
+// Array.prototype.isNotEmpty = function () {
+//   return this.length > 0;
+// };
 
 (() => {
-  const popupContainerName = 'ytd-popup-container';
-  const modalContainerName = 'paper-dialog';
-  const confirmButtonId = 'confirm-button';
+  window.videoContainerSelector = '#container.ytd-player';
+  window.videoSkipButtonSelector = '.ytp-ad-skip-button';
 
-  const clickAcceptButton = () => {
-    const confirmButton = document.querySelector(
-      `${modalContainerName} #${confirmButtonId}`,
-    );
-    confirmButton && confirmButton.click();
-  };
+  if (!!window.videoContainerSelector) {
+    window.videoContainerSelector = document.querySelector(videoContainerSelector);
+    window.videoSkipAddButton = videoContainerSelector.querySelector(videoSkipButtonSelector);
+  }
 
-  !window.modalObserver &&
-    (() => {
-      const popupContainer = document.querySelector(popupContainerName);
+  window.videoSkipAddButton && window.videoSkipAddButton.click();
 
-      window.modalObserver = new MutationObserver(mutations => {
-        const modalExists = [...mutations]
-          .filter(mutationList =>
-            [...mutationList.addedNodes]
-              .filter(
-                mutation =>
-                  mutation.nodeName.toLowerCase() === modalContainerName,
-              )
-              .isNotEmpty(),
-          )
-          .isNotEmpty();
+  // !window.modalObserver &&
+  //   (() => {
+  //     const popupContainer = document.querySelector(popupContainerName);
 
-        modalExists && clickAcceptButton();
-      });
+  //     window.modalObserver = new MutationObserver(mutations => {
+  //       const modalExists = [...mutations]
+  //         .filter(mutationList =>
+  //           [...mutationList.addedNodes]
+  //             .filter(
+  //               mutation =>
+  //                 mutation.nodeName.toLowerCase() === modalContainerName,
+  //             )
+  //             .isNotEmpty(),
+  //         )
+  //         .isNotEmpty();
 
-      window.modalObserver.observe(popupContainer, {
-        childList: true,
-        subtree: true,
-        attributes: false,
-        characterData: false,
-      });
-    })();
+  //       modalExists && clickAcceptButton();
+  //     });
+
+  //     window.modalObserver.observe(popupContainer, {
+  //       childList: true,
+  //       subtree: true,
+  //       attributes: false,
+  //       characterData: false,
+  //     });
+  //   })();
 })();
